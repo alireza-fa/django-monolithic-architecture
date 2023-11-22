1d4073266844882b34a6f11064be8d35004d7080
alireza-fa <alirezafeyze44@gmail.com>
2023-11-22 12:05:11 +0330

    apps struct

--- config/settings.py
+++ config/settings.py
@@ -53,6 +53,7 @@ if DEBUG:
     THIRD_PARTY_APPS.append('debug_toolbar')
 
 LOCAL_APPS = [
+    'apps.jwt_auth.apps.JwtAuthConfig',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
