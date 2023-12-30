2fa86e6d93750492fba8abf02cf7843e38261a84
alireza-fa <alirezafeyze44@gmail.com>
2023-12-30 08:40:04 +0330

    update base response

--- apps/api/response.py
+++ apps/api/response.py
@@ -1,5 +1,11 @@
+from typing import Dict
+
 from rest_framework.response import Response
 
 
-def base_response(status_code: int, result: object, success: bool, code: int) -> Response:
-    return Response(data={"result": result, success: success, code: code}, status=status_code)
+def base_response(status_code: int, result: Dict, success: bool, code: int) -> Response:
+    return Response(data={"result": result, "success": success, "code": code}, status=status_code)
+
+
+def base_response_with_error(status_code: int, result: Dict, success: bool, code: int, error) -> Response:
+    return Response(data={"result": result, "success": success, "code": code, "error": error}, status=status_code)
