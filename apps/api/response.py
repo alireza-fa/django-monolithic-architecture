9105b5ef78ab7543225720b3f7e5d6e4508c093b
alireza-fa <alirezafeyze44@gmail.com>
2024-01-03 20:26:26 +0330

    update response.py

--- apps/api/response.py
+++ apps/api/response.py
@@ -1,11 +1,17 @@
-from typing import Dict
+from typing import Dict, List
 
 from rest_framework.response import Response
+from rest_framework.serializers import ValidationError
 
 
-def base_response(status_code: int, result: Dict, success: bool, code: int) -> Response:
+def base_response(status_code: int, success: bool, code: int, result: Dict | None = None) -> Response:
     return Response(data={"result": result, "success": success, "code": code}, status=status_code)
 
 
-def base_response_with_error(status_code: int, result: Dict, success: bool, code: int, error) -> Response:
+def base_response_with_error(status_code: int, success: bool, code: int, error: str, result: Dict) -> Response:
+    return Response(data={"result": result, "success": success, "code": code, "error": error}, status=status_code)
+
+
+def base_response_with_validation_error(
+        status_code: int, success: bool, code: int, error: ValidationError, result: Dict) -> Response:
     return Response(data={"result": result, "success": success, "code": code, "error": error}, status=status_code)
