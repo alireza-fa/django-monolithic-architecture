3ab03024135026ea43a223a1ecbf012a374cd1bb
alireza-fa <alirezafeyze44@gmail.com>
2024-01-24 06:35:06 +0330

    update base responses

--- apps/api/response.py
+++ apps/api/response.py
@@ -4,14 +4,16 @@ from rest_framework.response import Response
 from rest_framework.serializers import ValidationError
 
 from apps.api.response_code import ERROR_TRANSLATION
+from apps.api.response_code import BAD_REQUEST
 
 
-def base_response(status_code: int, success: bool, code: int, result: Dict | None = None) -> Response:
+def base_response(status_code: int, code: int, success: bool = True, result: Dict | None = None) -> Response:
     return Response(data={"result": result, "success": success, "code": code}, status=status_code)
 
 
 def base_response_with_error(
-        status_code: int, success: bool, code: int, error: str | None = None, result: Dict | None = None) -> Response:
+        status_code: int, code: int, success: bool = False,
+        error: str | None = None, result: Dict | None = None) -> Response:
     if error:
         return Response(data={"result": result, "success": success, "code": code, "error": error}, status=status_code)
     return Response(data={"result": result, "success": success, "code": code, "error": ERROR_TRANSLATION[code]},
@@ -19,5 +21,6 @@ def base_response_with_error(
 
 
 def base_response_with_validation_error(
-        status_code: int, success: bool, code: int, error: ValidationError, result: Dict | None = None) -> Response:
+        status_code: int, error: ValidationError, code: int = BAD_REQUEST,
+        success: bool = False, result: Dict | None = None) -> Response:
     return Response(data={"result": result, "success": success, "code": code, "error": error}, status=status_code)
