08ec4c6440dd66fdf58849936b60c982ff068bbb
alireza-fa <alirezafeyze44@gmail.com>
2024-03-14 21:04:28 +0330

    add dummy logger

---
+++ apps/pkg/logger/dummy/logger.py
@@ -0,0 +1,24 @@
+import functools
+from typing import Dict
+
+from apps.pkg.logger.base import Log
+
+
+class DummyLogger(Log):
+
+    def debug(self, message: str, category: str, sub_category: str, properties: Dict):
+        print(f"message: {message}, category: {category}, sub category: {sub_category}, properties: {properties}")
+
+    def info(self, message: str, category: str, sub_category: str, properties: Dict):
+        print(f"message: {message}, category: {category}, sub category: {sub_category}, properties: {properties}")
+
+    def warn(self, message: str, category: str, sub_category: str, properties: Dict):
+        print(f"message: {message}, category: {category}, sub category: {sub_category}, properties: {properties}")
+
+    def error(self, message: str, category: str, sub_category: str, properties: Dict):
+        print(f"message: {message}, category: {category}, sub category: {sub_category}, properties: {properties}")
+
+
+@functools.cache
+def get_dummy_logger_once():
+    return DummyLogger()
