57c7612ee6ed758546079d22fe6ba0417e0f5e1e
alireza-fa <alirezafeyze44@gmail.com>
2024-03-15 12:33:17 +0330

    register api

--- apps/api/response_code.py
+++ apps/api/response_code.py
@@ -1,13 +1,18 @@
 OK = 2000
+CREATED = 2010
 NOT_ACCEPTABLE = 4060
 BAD_REQUEST = 4010
 USER_NOT_FOUND = 4040
 TOO_MANY_REQUEST = 4290
+INTERNAL_SERVER_ERROR = 5000
 
 INVALID_OTP = 4061
 
 ERROR_TRANSLATION = {
+    OK: "Ok",
+    CREATED: "Created a row",
     INVALID_OTP: "Invalid code",
     TOO_MANY_REQUEST: "Please try again later",
-    USER_NOT_FOUND: "User with this information not found"
+    USER_NOT_FOUND: "User with this information not found",
+    INTERNAL_SERVER_ERROR: "Interval server error",
 }
--- apps/authentication/api/http/v1/register.py
+++ apps/authentication/api/http/v1/register.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import get_user_model
 
-from rest_framework import serializers
+from rest_framework import serializers, status
 from rest_framework.views import APIView
 
+from apps.api import response_code
 from apps.api.response import base_response, base_response_with_error, base_response_with_validation_error
+from apps.authentication.services.register import register_user
 
 User = get_user_model()
 
@@ -21,6 +23,14 @@ class RegisterView(APIView):
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
-            pass
+            try:
+                vd = serializer.validated_data
+                token = register_user(request=request, username=vd["username"],
+                                      email=vd["email"], password=vd["password"])
+            except:
+                return base_response_with_error(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                                code=response_code.INTERNAL_SERVER_ERROR)
+
+            return base_response(status_code=status.HTTP_201_CREATED, code=response_code.CREATED, result=token)
 
         return base_response_with_validation_error(error=serializer.errors)
