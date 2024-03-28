9b82fa5286a95d02421d8a8d93bf5d5d3ef4a57a
alireza-fa <alirezafeyze44@gmail.com>
2024-03-28 12:48:55 +0330

    delete api_docs config from settings.py

--- config/apps_settings/api_docs.py
+++
@@ -1,7 +0,0 @@
-SPECTACULAR_SETTINGS = {
-    'TITLE': 'Project API',
-    'DESCRIPTION': 'project description',
-    'VERSION': '1.0.0',
-    'SERVE_INCLUDE_SCHEMA': False,
-    # OTHER SETTINGS
-}
